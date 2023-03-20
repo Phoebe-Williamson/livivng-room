1ddb6a24a42d50ada5836c8f73541377cbd604d6
Phoebe <100629391+Phoebe-Williamson@users.noreply.github.com>
2023-03-20 14:09:00 +1300

    made the files

---
+++ lamp.java
@@ -0,0 +1,36 @@
+import ecs100.*;
+import java.awt.Color;
+/**
+ * creates a lamp
+ * it can tirn on and off
+ * change colour too when on
+ *
+ * @author (Phoebe WIlliamosno)
+ * @version (20/3/23)
+ */
+public class lamp
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class lamp
+     */
+    public lamp()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ lamps.java
@@ -0,0 +1,34 @@
+import ecs100.*;
+import java.awt.Color;
+/**
+ * driver class for the lamp support class
+ *
+ * @author (Phoeeb williamosn)
+ * @version (20/3/23)
+ */
+public class lamps
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class lamps
+     */
+    public lamps()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
